e5c9294b1f76443650d6d933ee85fe886ea0f55c
AlexanderBelikov <belikov.it@gmail.com>
2019-01-18 09:07:05 +0500

    CloudFireStore trying set permissions

--- app/src/main/java/su/zzz/locater/LocaterActivity.java
+++ app/src/main/java/su/zzz/locater/LocaterActivity.java
@@ -1,6 +1,8 @@
 package su.zzz.locater;
 
 import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,13 +11,27 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LocaterActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = LocaterActivity.class.getSimpleName();
     private DrawerLayout mDrawerLayout;
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
@@ -46,7 +62,76 @@ public class LocaterActivity extends AppCompatActivity implements NavigationView
         if(mUser!=null){
             TextView viewUserName = mNavigationView.getHeaderView(0).findViewById(R.id.userName);
             viewUserName.setText(mUser.getEmail());
+            ConnectDevice();
         }
+    }
+
+    private void ConnectDevice() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String deviceAndroidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        String deviceId = Build.ID+"-"+deviceAndroidId;
+
+        CollectionReference refDevices = db.collection(deviceId);
+        Map<String, Object> deviceSettings = new HashMap<>();
+        deviceSettings.put("manager", mUser.getUid());
+        deviceSettings.put("device_id", deviceAndroidId);
+
+        try {
+            refDevices.document("settings").set(deviceSettings, SetOptions.merge())
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.i(TAG, "onSuccess: ");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.i(TAG, "onFailure: ");
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: ", e);
+        }
+
+        Map<String, Object> deviceLocations = new HashMap<>();
+//        deviceLocations.put("location", "zzz");
+
+        try {
+            refDevices.document("locations").set(deviceLocations, SetOptions.merge())
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.i(TAG, "onSuccess: ");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.i(TAG, "onFailure: ");
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: ", e);
+        }
+    }
+
+    private void OnFirebaseAuth() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference refDevices = db.collection("Devices");
+        refDevices.whereEqualTo("Manager", mUser.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.i(TAG, "onComplete: "+task.isSuccessful());
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+//        DocumentReference deviceReference = db.collection("Devices").document(Build.DISPLAY);
     }
 
     @Override
@@ -58,8 +143,6 @@ public class LocaterActivity extends AppCompatActivity implements NavigationView
                     startActivity(intent);
                 } else {
                     mDrawerLayout.openDrawer(GravityCompat.START);
-//                    mAuth.signOut();
-//                    mUser = null;
                 }
                 return true;
         }
